fc44f5fb612cd844ab9f0febc259e06fe6908e18
Sabari6601 <sabari.radhakrishnan21@gmail.com>
2024-08-13 20:33:19 +0530

    creating tables using java

---
+++ forloop.java
@@ -0,0 +1,17 @@
+
+package forloop;
+import java.util.*;
+public class Forloop {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("enter a number");
+        int n=sc.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(n+"x"+i+"="+n*i);
+        }
+   
+    }
+    
+}
